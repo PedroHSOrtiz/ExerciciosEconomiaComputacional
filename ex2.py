30e6161c845036ea14ee03ab00d2b2b5d3b8b4c3
PedroHSOrtiz <phsortiz@gmail.com>
2026-03-22 18:01:59 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,31 @@
+# Programa 2 + 3
+# Descrição: Este programa armazena os números 2 e 3 e retorna a soma dos dois
+# Autor: Pedro H. S. Ortiz
+# Data: 22/03/2026
+# Versão: 0.0.1
+# Notas da versão:
+
+
+
+# Alocação de Memória
+
+numero1 = 0
+
+numero2 = 0
+
+soma = 0
+
+# Entrada de dados
+
+numero1 = int(2)
+
+numero2 = int(3)
+
+# Processamento: cálculo da soma
+
+soma = numero1 + numero2
+
+
+# Saída de dados
+
+print(soma)
